282a1c035c508cf2649b1822dc86cc2f5aefa3df
Lucas Duarte <lukazds2016@gmail.com>
2022-07-29 20:56:00 -0300

    Creating table cities

--- main.sql
+++ main.sql
@@ -5,3 +5,11 @@ CREATE DATABASE "dataPractice";
 -- Creating table states;
 
 CREATE TABLE states (id SERIAL PRIMARY KEY, name VARCHAR(140) NOT NULL);
+
+-- Creating table cities;
+
+CREATE TABLE cities (
+	id SERIAL PRIMARY KEY, 
+	name VARCHAR(140) NOT NULL, 
+	"stateId" INTEGER NOT NULL REFERENCES "states"("id"));
+
