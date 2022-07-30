f928242cc8ce3a4547d36f3c9336ed53acbd9d1f
Lucas Duarte da Silva <102393984+LukazDs@users.noreply.github.com>
2022-07-29 22:33:39 -0300

    Update main.sql

--- main.sql
+++ main.sql
@@ -4,19 +4,19 @@ CREATE DATABASE "dataPractice";
 
 -- Creating table states;
 
-CREATE TABLE states (id SERIAL PRIMARY KEY, name VARCHAR(140) UNIQUE NOT NULL);
+CREATE TABLE states (id SERIAL NOT NULL PRIMARY KEY, name VARCHAR(140) UNIQUE NOT NULL);
 
 -- Creating table cities
 
 CREATE TABLE cities (
-id SERIAL PRIMARY KEY, 
+id SERIAL NOT NULL PRIMARY KEY, 
 name VARCHAR(140) UNIQUE NOT NULL, 
 "stateId" INTEGER NOT NULL REFERENCES "states"("id"));
 
 -- Creating table customers
 
 CREATE TABLE customers(
-id SERIAL PRIMARY KEY,
+id SERIAL NOT NULL PRIMARY KEY,
 "fullName" VARCHAR(255) UNIQUE NOT NULL,
 cpf BIGINT UNIQUE NOT NULL,
 email TEXT UNIQUE NOT NULL,
@@ -25,7 +25,7 @@ password TEXT NOT NULL);
 -- Creating table customerPhones
 
 CREATE TABLE "customerPhones" (
-id SERIAL PRIMARY KEY, 
+id SERIAL NOT NULL PRIMARY KEY, 
 "customerId" INTEGER REFERENCES "customers"("id") NOT NULL,
 number BIGINT UNIQUE NOT NULL,
 type TEXT DEFAULT 'landline');
@@ -33,7 +33,7 @@ type TEXT DEFAULT 'landline');
 -- Creating table customerAddress
 
 CREATE TABLE "customerAddress" (
-id SERIAL PRIMARY KEY,
+id SERIAL NOT NULL PRIMARY KEY,
 "customerId" INTEGER REFERENCES "customers"("id") NOT NULL,
 street TEXT NOT NULL,
 number INTEGER NOT NULL,
@@ -44,7 +44,7 @@ dataPractice "cityId" INTEGER REFERENCES "cities"("id") NOT NULL);
 -- Creating table bankAccount
 
 CREATE TABLE "bankAccount" (
-id SERIAL PRIMARY KEY,
+id SERIAL NOT NULL PRIMARY KEY,
 "customerId" INTEGER REFERENCES "customers"("id") NOT NULL,
 "accountNumber" BIGINT UNIQUE NOT NULL,
 agency BIGINT NOT NULL,
@@ -54,7 +54,7 @@ agency BIGINT NOT NULL,
 -- Creating table transactions
 
 CREATE TABLE transactions (
-id SERIAL PRIMARY KEY,
+id SERIAL NOT NULL PRIMARY KEY,
 "bankAccountId" INTEGER REFERENCES "bankAccount"("id") NOT NULL,
 amount INTEGER NOT NULL,
 type TEXT DEFAULT 'deposit' NOT NULL,
@@ -65,10 +65,13 @@ cancelled BOOLEAN NOT NULL );
 -- Creating table creditCards
 
 CREATE TABLE "creditCards" (
-id SERIAL PRIMARY KEY,
+id SERIAL NOT NULL PRIMARY KEY,
 "bankAccountId" INTEGER REFERENCES "bankAccount"("id") NOT NULL,
 name VARCHAR(140) UNIQUE NOT NULL,
-number BIGINT NOT NULL, "securityCode" BIGINT NOT NULL,
-"expirationMonth" TEXT NOT NULL, "expirationYear" INTEGER NOT NULL,
-password TEXT NOT NULL, "limit" INTEGER NOT NULL);
+number BIGINT NOT NULL, 
+"securityCode" BIGINT NOT NULL,
+"expirationMonth" INTEGER NOT NULL, 
+"expirationYear" INTEGER NOT NULL,
+password TEXT NOT NULL, 
+"limit" INTEGER NOT NULL);
 
