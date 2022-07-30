2baa1185bccd5dcb3c5fa799f7f3f75e327413cd
Lucas Duarte <lukazds2016@gmail.com>
2022-07-29 21:10:43 -0300

    Creating table customers'

--- main.sql
+++ main.sql
@@ -4,12 +4,21 @@ CREATE DATABASE "dataPractice";
 
 -- Creating table states;
 
-CREATE TABLE states (id SERIAL PRIMARY KEY, name VARCHAR(140) NOT NULL);
+CREATE TABLE states (id SERIAL PRIMARY KEY, name VARCHAR(140) UNIQUE NOT NULL);
 
 -- Creating table cities;
 
 CREATE TABLE cities (
-	id SERIAL PRIMARY KEY, 
-	name VARCHAR(140) NOT NULL, 
-	"stateId" INTEGER NOT NULL REFERENCES "states"("id"));
+id SERIAL PRIMARY KEY, 
+name VARCHAR(140) UNIQUE NOT NULL, 
+"stateId" INTEGER NOT NULL REFERENCES "states"("id"));
+
+-- Creating table customers;
+
+CREATE TABLE customers(
+id SERIAL PRIMARY KEY,
+"fullName" VARCHAR(255) UNIQUE NOT NULL,
+cpf BIGINT UNIQUE NOT NULL,
+email TEXT UNIQUE NOT NULL,
+password TEXT NOT NULL);
 
